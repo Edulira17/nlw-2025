d811d4a058ad9044356c47e57d6d26a60f553442
Eduardo Lira <eduardoliracunha17@hotmail.com>
2025-07-10 21:07:27 -0400

    testa componentes do shadcn

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,4 +1,14 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <Button variant={"secondary"}>Hello World</Button>
+      <Button variant={"destructive"}>Hello World</Button>
+      <Button variant={"ghost"}>Hello World</Button>
+      <Button variant={"link"}>Hello World</Button>
+      <Button variant={"secondary"}>Hello World</Button>
+    </div>
+  )
 }
 
