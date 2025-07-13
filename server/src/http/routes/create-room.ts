f08ec549a91bad7b24a9ac32b62f2c08b5fcea30
Eduardo Lira <eduardoliracunha17@hotmail.com>
2025-07-13 10:48:22 -0400

    feat: adiciona rota de criação de nova sala

---
+++ server/src/http/routes/create-room.ts
@@ -0,0 +1,36 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import {z} from "zod/v4";
+import { db } from "../../db/connection.ts";
+import { schema } from "../../db/schema/index.ts";
+
+// Create new room
+export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    "/rooms",
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(1),
+          description: z.string().optional(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, description } = request.body;
+
+      // Após inserir a nova sala no BD, a função returning() faz com que o banco retorne os dados das linhas inseridas.
+      const result = await db.insert(schema.rooms).values({
+        name,
+        description,
+      }).returning();
+
+      const insertedRoom = result[0]
+
+      if(!insertedRoom){
+        throw new Error('Failed to create new room')
+      }
+
+      return reply.status(201).send({roomId: insertedRoom.id})
+    }
+  );
+};
