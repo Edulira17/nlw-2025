bf09870f81880dc91b415132dba0de3d3239d7d2
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-07-16 19:03:04 -0400

    feat: adiciona rota para criação de perguntas em salas e atualiza rota de obtenção de salas para incluir contagem de perguntas

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
+import { count, eq } from "drizzle-orm";
 
 // route que retorna todas as salas
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
@@ -9,9 +10,13 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
     const results = await db
       .select({
         id: schema.rooms.id,
+        createdAt: schema.rooms.createdAt,
         name: schema.rooms.name,
+        questionsCount: count(schema.questions.id)
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id, schema.rooms.name)
       .orderBy(schema.rooms.createdAt);
 
     return results;
--- server/src/server.ts
+++ server/src/server.ts
@@ -14,6 +14,7 @@ import { env } from "./env.ts";
 import { createRoomRoute } from "./http/routes/create-room.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { getRoomQuestions } from "./http/routes/get-room-question.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
 
 // permite que o Fastify “entenda” os esquemas do Zod e use esses tipos para validar auto, fornecer tipagem auto no typescript
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -32,6 +33,7 @@ app.get("/health", () => {
 app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestions);
+app.register(createQuestionRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   // biome-ignore lint/suspicious/noConsole: only used in dev
